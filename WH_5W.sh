f40ad2cd5b122108b4ba444cf89d08f5a159d1f1
wh891 <58629942+wh891@users.noreply.github.com>
2019-12-07 22:08:45 +0800

    Add files via upload

---
+++ WH_5W.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+clear
+
+read -p "masukan nomor anda untuk login :" nama
+
+echo " anda telah login" $nama
+
+echo "\33[1;33m ADA YG MAU GRUP WA CYBER"
+
+echo " "
+
+echo "\33[31;1m ini gaes https://chat.whatsapp.com/IGX1F5tnZKe8KG2sGzITtN"
+
+echo " "
+
+echo "\33[33;1m copy lalu salin ke google"
+
+#
+
+sleep 100
